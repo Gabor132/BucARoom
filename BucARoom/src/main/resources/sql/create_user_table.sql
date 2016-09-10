b5a62071bfefb0bf499b13998bddcef7a243aa3e
Dragos <gabordragos@gmail.com>
2016-09-10 15:57:27 +0300

    Added create user sql.

---
+++ BucARoom/src/main/resources/sql/create_user_table.sql
@@ -0,0 +1,9 @@
+
+/**
+ * Author:  Dragos
+ * Created: Sep 10, 2016
+ */
+
+create table users (id bigint(20) auto_increment primary key, username varchar(255) not null,
+ password varchar(255) not null, lastName varchar(255) not null,
+ firstName varchar(255) not null, email varchar(255) not null);
