b7f3aa9a1f18f9eeae89d5d4711313fc8f3c7926
Dragos <gabordragos@gmail.com>
2016-09-11 15:32:07 +0300

    Added some null verifications and stopped the form from reloading the page.

--- BucARoom/src/main/java/com/BucARoom/login/controller/LoginController.java
+++ BucARoom/src/main/java/com/BucARoom/login/controller/LoginController.java
@@ -8,8 +8,6 @@ package com.BucARoom.login.controller;
 import com.BucARoom.user.entitie.Users;
 import com.BucARoom.user.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.RestController;
